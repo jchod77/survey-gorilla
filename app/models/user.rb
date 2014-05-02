49dd76ac803331d21055eb24d2ddc592b6fedd56
Cynaria <Elizabeth.J.Cruz@gmail.com>
2014-05-02 15:52:56 -0500

    Model changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
 	has_many :responses
-	has_many :
 	has_many :created_surveys, :class_name => "Survey", :foreign_key => :creator_id
 	validates :name, uniqueness: true
 end
